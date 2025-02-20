b0f26a5ebdba8e3208791bd81b89f93e14c119c4
Lorenzo Perino <lorenzo.perino6694@gmail.com>
2025-02-20 15:50:49 +0000

    correcting percentile rounding

--- 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_monthly_fare_p95.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_monthly_fare_p95.sql
@@ -10,13 +10,12 @@ select
     year,
     month,
     fare_amount,
-    round(
-            percent_rank() over (
+    floor(
+        percent_rank() over (
             partition by service_type, year, month 
             order by fare_amount
-        ),
-        2
-        ) as percentile
+        ) * 100 
+    ) / 100 as percentile
 from {{ ref('dim_taxi_trips') }}
 where 
     fare_amount > 0
