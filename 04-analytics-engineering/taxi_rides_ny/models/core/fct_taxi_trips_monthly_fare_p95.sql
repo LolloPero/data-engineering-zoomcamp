0f9eee6004d19f48fd1c27fdca79201d893ec44c
Lorenzo Perino <lorenzo.perino6694@gmail.com>
2025-02-20 15:07:31 +0000

    fct_taxi_trips_monthly_fare_p95.sql

---
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_monthly_fare_p95.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+
+select 
+    service_type,
+    year,
+    month,
+    fare_amount,
+    percent_rank() over (
+        partition by service_type, year, month 
+        order by fare_amount
+    ) as percentile
+from {{ ref('dim_taxi_trips') }}
+where 
+    fare_amount > 0
+    and trip_distance > 0
+    and payment_type_description in ('Cash', 'Credit Card')
+order by service_type, year, month, fare_amount
