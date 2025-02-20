61e1f2f195018a854b40a7e83d5458d240ce6e7c
Lorenzo Perino <lorenzo.perino6694@gmail.com>
2025-02-20 16:39:52 +0000

    final rounding

--- 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_monthly_fare_p95.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_monthly_fare_p95.sql
@@ -5,20 +5,32 @@
 }}
 
 
-select 
-    service_type,
-    year,
-    month,
-    fare_amount,
-    floor(
-        percent_rank() over (
-            partition by service_type, year, month 
-            order by fare_amount
-        ) * 100 
-    ) / 100 as percentile
-from {{ ref('dim_taxi_trips') }}
-where 
-    fare_amount > 0
-    and trip_distance > 0
-    and payment_type_description in ('Cash', 'Credit Card')
-order by service_type, year, month, fare_amount
+with percentile_table as (
+    select 
+        service_type,
+        year,
+        month,
+        fare_amount,
+        ROUND(
+                percent_rank() over (
+                                        partition by service_type, year, month 
+                                        order by fare_amount
+                                    )
+            ,2) as percentile
+    from {{ ref('dim_taxi_trips') }}
+        where 
+        fare_amount > 0
+        and trip_distance > 0
+        and payment_type_description in ('Cash', 'Credit Card')
+)
+select  service_type, 
+        year, 
+        month,  
+        percentile, 
+        AVG(fare_amount) as fare_amount
+from 
+        percentile_table
+group by 
+        service_type, year, month,  percentile
+order by 
+        service_type, year desc, month,  percentile
